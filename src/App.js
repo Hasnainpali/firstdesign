c01df2954694aa0466cdfe170051286a4533c11f
Hasnain Iqbal <hasnainpali321@gmail.com>
2023-12-16 18:27:20 +0500

    change files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Form from "./Components/Form";
-import InputForm from "./Components/InputForm";
+
 
 
 function App() {
   return (
     <>
        <Form/>
-       {/* <InputForm/> */}
+     
     </> 
   );
 }
